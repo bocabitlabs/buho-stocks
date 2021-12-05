a51477bcc78382936559dbef91ba7542cb33b3b5
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2021-12-05 08:07:48 +0000

    Add exchange rates type

---
+++ client/src/contexts/exchange-rates.ts
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { IExchangeRate } from "types/exchange-rate";
+
+export type ExchangeRatesContextType = {
+  isLoading: boolean;
+  exchangeRate: IExchangeRate | null;
+  get: (fromCode: string, toCode: string, exchangeDate: string) => void;
+};
+
+export const defaultValue: ExchangeRatesContextType = {
+  isLoading: false,
+  exchangeRate: null,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  get: (fromCode: string, toCode: string, exchangeDate: string) => undefined
+};
+
+export const ExchangeRatesContext =
+  createContext<ExchangeRatesContextType>(defaultValue);
