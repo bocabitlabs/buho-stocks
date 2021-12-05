513013e5ec9500ee1e78d4f7d8271a8bf4ef3c39
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2021-12-05 07:51:01 +0000

    Add support for rights transactions

--- backend/rights_transactions/models.py
+++ backend/rights_transactions/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from buho_backend.transaction_types import TransactionType
 from companies.models import Company
 from shares_transactions.models import Transaction
 
 
 class RightsTransaction(Transaction):
+    type = models.CharField(
+        choices=TransactionType.choices, default=TransactionType.BUY, max_length=10
+    )
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, related_name="rights_transactions"
     )
+
+    def __str___(self):
+        return f"{self.type} - {self.count} - {self.gross_price_per_share} ({self.total_commission}"
--- backend/rights_transactions/views.py
+++ backend/rights_transactions/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.authentication import (
-    SessionAuthentication,
+    BasicAuthentication,
     TokenAuthentication,
 )
 from rest_framework.permissions import IsAuthenticated
@@ -14,19 +14,19 @@ from rights_transactions.serializers import RightsTransactionSerializer
 class RightsTransactionsListAPIView(APIView):
     """Get all the shares transactions from a user's company"""
 
-    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     # 1. List all
     @swagger_auto_schema(tags=["company_rights"])
     def get(self, request, company_id, *args, **kwargs):
-        """
-        List all the company items for given requested user
-        """
+
         elements = RightsTransaction.objects.filter(
             user=request.user.id, company=company_id
         )
-        serializer = RightsTransactionSerializer(elements, many=True)
+        serializer = RightsTransactionSerializer(
+            elements, many=True, context={"request": request}
+        )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     # 2. Create
@@ -38,18 +38,24 @@ class RightsTransactionsListAPIView(APIView):
         Create a shares transaction with given data
         """
         data = {
-            "name": request.data.get("name"),
             "count": request.data.get("count"),
             "color": request.data.get("color"),
             "exchange_rate": request.data.get("exchange_rate"),
             "transaction_date": request.data.get("transaction_date"),
             "type": request.data.get("type"),
-            "price_per_share": request.data.get("price_per_share"),
+            "gross_price_per_share": request.data.get("gross_price_per_share"),
+            "gross_price_per_share_currency": request.data.get(
+                "gross_price_per_share_currency"
+            ),
             "total_commission": request.data.get("total_commission"),
+            "total_commission_currency": request.data.get("total_commission_currency"),
             "notes": request.data.get("notes"),
             "company": company_id,
         }
-        serializer = RightsTransactionSerializer(data=data)
+
+        serializer = RightsTransactionSerializer(
+            data=data, context={"request": request}
+        )
         if serializer.is_valid():
             serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -60,10 +66,10 @@ class RightsTransactionsListAPIView(APIView):
 class RightsTransactionDetailAPIView(APIView):
     """Operations for a single Shares Transaction"""
 
-    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get_object(self, transaction_id, company_id, user_id):
+    def get_object(self, company_id, transaction_id, user_id):
         """
         Get a market object from a user given the portfolio id
         """
@@ -75,12 +81,14 @@ class RightsTransactionDetailAPIView(APIView):
             return None
 
     # 3. Retrieve
-    @swagger_auto_schema(tags=["company_rights"])
+    @swagger_auto_schema(tags=["company_shares"])
     def get(self, request, company_id, transaction_id, *args, **kwargs):
         """
         Retrieve the company item with given company_id
         """
-        instance = self.get_object(transaction_id, company_id, request.user.id)
+        print(company_id)
+        print(transaction_id)
+        instance = self.get_object(company_id, transaction_id, request.user.id)
         if not instance:
             return Response(
                 {"res": "Object with transaction id does not exists"},
@@ -92,7 +100,7 @@ class RightsTransactionDetailAPIView(APIView):
 
     # 4. Update
     @swagger_auto_schema(
-        tags=["company_shares"], request_body=RightsTransactionSerializer
+        tags=["company_rights"], request_body=RightsTransactionSerializer
     )
     def put(self, request, company_id, transaction_id, *args, **kwargs):
         """
@@ -105,19 +113,22 @@ class RightsTransactionDetailAPIView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
         data = {
-            "name": request.data.get("name"),
             "count": request.data.get("count"),
             "color": request.data.get("color"),
             "exchange_rate": request.data.get("exchange_rate"),
             "transaction_date": request.data.get("transaction_date"),
             "type": request.data.get("type"),
-            "price_per_share": request.data.get("price_per_share"),
+            "gross_price_per_share": request.data.get("gross_price_per_share"),
+            "gross_price_per_share_currency": request.data.get(
+                "gross_price_per_share_currency"
+            ),
             "total_commission": request.data.get("total_commission"),
+            "total_commission_currency": request.data.get("total_commission_currency"),
             "notes": request.data.get("notes"),
             "company": company_id,
         }
         serializer = RightsTransactionSerializer(
-            instance=instance, data=data, partial=True
+            instance=instance, data=data, partial=True, context={"request": request}
         )
         if serializer.is_valid():
             serializer.save()
@@ -128,7 +139,7 @@ class RightsTransactionDetailAPIView(APIView):
     @swagger_auto_schema(tags=["company_rights"])
     def delete(self, request, company_id, transaction_id, *args, **kwargs):
         """
-        Delete the item with given transaction id
+        Delete the company item with given transaction id
         """
         market_instance = self.get_object(transaction_id, company_id, request.user.id)
         if not market_instance:
