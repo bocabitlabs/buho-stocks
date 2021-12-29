d4efb3c2245b78e968b00eae4c758a7f1a0f57af
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2021-12-29 12:39:31 +0000

    Fix format

--- client/src/pages/sectors/SectorsListPage/components/SuperSectorsTable/SuperSectorsTable.tsx
+++ client/src/pages/sectors/SectorsListPage/components/SuperSectorsTable/SuperSectorsTable.tsx
@@ -13,7 +13,12 @@ export default function SuperSectorsTable() {
 
   const { t } = useTranslation();
   const { createError, createSuccess } = useContext(AlertMessagesContext);
-  const { loading, response, get, del: deleteSector } = useFetch("sectors/super");
+  const {
+    loading,
+    response,
+    get,
+    del: deleteSector,
+  } = useFetch("sectors/super");
 
   const confirmDelete = async (recordId: number) => {
     console.log(recordId);
