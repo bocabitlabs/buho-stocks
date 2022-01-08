33b7465345f44b19a50ff0821bbb3f7a2e9c746a
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2022-01-08 17:16:15 +0000

    Divide by 100 GBP stock prices from API

--- backend/stock_prices/services/yfinance_service.py
+++ backend/stock_prices/services/yfinance_service.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 import time
 import yfinance
+import logging
 from stock_prices.services.service_base import StockPriceServiceBase
+
+logger = logging.getLogger("buho_backend")
 
 
 class YFinanceStockPricesService(StockPriceServiceBase):
@@ -13,9 +16,13 @@ class YFinanceStockPricesService(StockPriceServiceBase):
         company = yfinance.Ticker(ticker)
         company_info = company.info
 
+        price = round(company_info["regularMarketPrice"], 3)
+        if company_info["currency"] == "GBP":
+            price = price / 100
+
         return {
             "company_name": company_info["shortName"],
-            "price": round(company_info["regularMarketPrice"], 3),
+            "price": price,
             "price_currency": company_info["currency"],
             "ticker": ticker,
             "transaction_date": datetime.now().strftime("%Y-%m-%d"),
@@ -23,28 +30,41 @@ class YFinanceStockPricesService(StockPriceServiceBase):
 
     def get_historical_data(self, ticker: str, start_date: str, end_date: str):
         prices = []
-        print(f"Get current data for {ticker} from {start_date} to {end_date}")
+        logger.debug(f"Get current data for {ticker} from {start_date} to {end_date}")
         company = yfinance.Ticker(ticker)
         time.sleep(2)
         company_info = self.get_current_data(ticker)
-        print(f"Get historical data for {ticker} from {start_date} to {end_date}")
+        logger.debug(
+            f"Get historical data for {ticker} from {start_date} to {end_date}"
+        )
         time.sleep(2)
-        result = company.history(start=start_date, end=end_date, period="1d", timeout=10)
-        print(result)
+        result = company.history(
+            start=start_date, end=end_date, period="1d", timeout=10
+        )
+        logger.debug(result)
         # print(type(result))
         # print(result)
         for col, row in result.iterrows():
             # result += max(row.B, row.C)
             # print(element)
-            print(col)
-            print(row["Close"])
-            data = {
-                "company_name": company_info["company_name"],
-                "price": round(row["Close"], 3),
-                "price_currency": company_info["price_currency"],
-                "ticker": ticker,
-                "transaction_date": col.to_pydatetime().strftime("%Y-%m-%d"),
-            }
-            prices.append(data)
-        print(f"Found {len(prices)} historical prices")
+            # print(col)
+            try:
+                price = round(row["Close"], 3)
+                if company_info["price_currency"] == "GBP":
+                    price = price / 100
+
+                data = {
+                    "company_name": company_info["company_name"],
+                    "price": price,
+                    "price_currency": company_info["price_currency"],
+                    "ticker": ticker,
+                    "transaction_date": col.to_pydatetime().strftime("%Y-%m-%d"),
+                }
+                prices.append(data)
+                logger.debug(f"Added {ticker} data {data}")
+            except KeyError as error:
+                logger.warning(
+                    f"Unable to append data for {ticker}. KeyError: {str(error)}"
+                )
+        logger.debug(f"Found {len(prices)} historical prices")
         return prices
