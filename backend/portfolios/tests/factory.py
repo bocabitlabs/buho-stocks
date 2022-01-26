12e7a0ce770837fd97dbe3b5e9c8c1a7c2f66953
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2022-01-26 20:32:36 +0000

    Use a currency from the currencies list

--- backend/portfolios/tests/factory.py
+++ backend/portfolios/tests/factory.py
@@ -1,5 +1,7 @@
+import random
 from factory import django, Faker, SubFactory
 from auth.tests.factory import UserFactory
+from currencies.models import get_all_currencies
 from portfolios.models import Portfolio
 
 
@@ -10,7 +12,7 @@ class PortfolioFactory(django.DjangoModelFactory):
     name = Faker("company")
     color = Faker("color")
     description = Faker("paragraph")
-    base_currency = Faker("currency_code")
+    base_currency = random.choice(get_all_currencies())["code"]
     country_code = Faker("country_code")
     hide_closed_companies = Faker("boolean")
 
