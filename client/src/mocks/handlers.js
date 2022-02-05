d9ded1460a57daba220cf83f508fedcbe9f8a0dc
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2022-02-05 08:31:55 +0000

    Add mocks for settings

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -1,11 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { rest } from "msw";
 import currenciesList from "./responses/currencies";
+import settingsMock from "./responses/settings";
 
 export const handlers = [
   rest.get("/api/v1/currencies/", (req, res, ctx) => {
     // If authenticated, return a mocked user details
     return res(ctx.status(200), ctx.json(currenciesList));
+  }),
+  rest.get("/api/v1/settings/", (req, res, ctx) => {
+    // If authenticated, return a mocked user details
+    return res(ctx.status(200), ctx.json(settingsMock));
   }),
 ];
 
---
+++ client/src/mocks/responses/settings.ts
@@ -0,0 +1,15 @@
+import { ISettings } from "types/settings";
+
+const settingsMock: ISettings = {
+  id: 1,
+  language: "en",
+  portfolioSortBy: "date",
+  companySortBy: "date",
+  mainPortfolio: "1",
+  portfolioDisplayMode: "grid",
+  companyDisplayMode: "grid",
+  dateCreated: "2020-01-01T00:00:00.000Z",
+  lastUpdated: "2020-01-01T00:00:00.000Z",
+};
+
+export default settingsMock;
