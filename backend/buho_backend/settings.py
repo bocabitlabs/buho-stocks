30a8712dcfa1eaa405c3e1c7a71b4c37bbee52ea
René Fernández <renefs87@gmail.com>
2022-05-13 23:18:41 +0200

    change static files path

--- backend/buho_backend/settings.py
+++ backend/buho_backend/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static-files/"
 STATIC_ROOT = "/app/static/"
 
 MEDIA_URL = "/media/"
