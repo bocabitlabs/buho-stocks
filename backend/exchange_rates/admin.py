02d92128c721840c84edc04785b25c43c5e9baae
René Fernández <renefs87@gmail.com>
2022-06-05 09:39:37 +0200

    add admin for exchange rates

--- backend/exchange_rates/admin.py
+++ backend/exchange_rates/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from exchange_rates.models import ExchangeRate
+from buho_backend.admin import BaseAdmin
 
 # Register your models here.
+@admin.register(ExchangeRate)
+class ExchangeRateAdmin(BaseAdmin):
+    list_display = ['id', 'exchange_from', 'exchange_to', 'exchange_date', 'last_updated']
+    search_fields = ['exchange_from', 'exchange_to']
