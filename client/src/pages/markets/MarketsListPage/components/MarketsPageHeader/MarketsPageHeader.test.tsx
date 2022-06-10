1453bdd832deef1e9b7999fa15f9924c3ed6df86
René Fernández <renefs87@gmail.com>
2022-06-10 13:00:57 +0200

    Add tests for Markets list page header

---
+++ client/src/pages/markets/MarketsListPage/components/MarketsPageHeader/MarketsPageHeader.test.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React from "react";
+import { screen } from "@testing-library/react";
+import MarketsPageHeader from "./MarketsPageHeader";
+import { renderWithRouterAndQueryClient } from "utils/test-utils";
+
+describe("CurrenciesPageHeader tests", () => {
+  it("renders expected texts", async () => {
+    renderWithRouterAndQueryClient(
+      <MarketsPageHeader>
+        <div>Page content</div>
+      </MarketsPageHeader>,
+    );
+
+    const element = screen.getAllByText(/Markets/i);
+    expect(element).toHaveLength(2);
+
+    const elements = screen.getByText(/Add market/i);
+    expect(elements).toBeInTheDocument();
+
+    const body = screen.getByText(/Page Content/i);
+    expect(body).toBeInTheDocument();
+  });
+});
