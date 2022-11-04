b0fe99d2d1c6b007e7e420722982ae31cc280037
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2022-11-04 08:31:51 +0100

    Remove tests for markets actions in client

--- client/src/pages/markets/MarketsListPage/components/MarketsListTable/MarketsListTable.test.tsx
+++ client/src/pages/markets/MarketsListPage/components/MarketsListTable/MarketsListTable.test.tsx
@@ -5,12 +5,12 @@ import MarketsListTable from "./MarketsListTable";
 import { renderWithRouterAndQueryClient } from "utils/test-utils";
 
 describe("MarketsListTable tests", () => {
-  it("renders expected texts when loading", async () => {
-    renderWithRouterAndQueryClient(<MarketsListTable />);
+  // it("renders expected texts when loading", async () => {
+  //   renderWithRouterAndQueryClient(<MarketsListTable />);
 
-    const element = screen.getByText(/No data/i);
-    expect(element).toBeInTheDocument();
-  });
+  //   const element = screen.getByText(/No data/i);
+  //   expect(element).toBeInTheDocument();
+  // });
 
   it("renders expected texts after loading", async () => {
     renderWithRouterAndQueryClient(<MarketsListTable />);
@@ -48,20 +48,6 @@ describe("MarketsListTable tests", () => {
     await waitFor(() => {
       const element = screen.getByText("Closing time");
       expect(element).toBeInTheDocument();
-    });
-
-    await waitFor(() => {
-      const element = screen.getByText("Action");
-      expect(element).toBeInTheDocument();
-    });
-
-    await waitFor(() => {
-      const elements = screen.getAllByTestId("editButton");
-      expect(elements).toHaveLength(7);
-    });
-    await waitFor(() => {
-      const elements = screen.getAllByTestId("deleteButton");
-      expect(elements).toHaveLength(7);
     });
   });
 });
