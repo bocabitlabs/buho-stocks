15f5db9753515156b6ef8fc099b8714dda48b198
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-03-05 18:07:12 +0100

    Fix flake8 isues on companies models

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
-from sectors.models import Sector
 from markets.models import Market
 from portfolios.models import Portfolio
+from sectors.models import Sector
+
 
 def user_directory_path(instance, filename):
-
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return f"user_{instance.user.id}/{filename}"
+
 
 # Create your models here.
 class Company(models.Model):
@@ -29,25 +30,16 @@ class Company(models.Model):
     base_currency = models.CharField(max_length=50)
     dividends_currency = models.CharField(max_length=50)
 
-    sector = models.ForeignKey(
-        Sector, on_delete=models.RESTRICT, related_name="companies"
-    )
-    market = models.ForeignKey(
-        Market, on_delete=models.RESTRICT, related_name="companies"
-    )
-    portfolio = models.ForeignKey(
-        Portfolio, on_delete=models.CASCADE, related_name="companies"
-    )
-    logo = models.ImageField(upload_to = user_directory_path, blank=True, null=True)
+    sector = models.ForeignKey(Sector, on_delete=models.RESTRICT, related_name="companies")
+    market = models.ForeignKey(Market, on_delete=models.RESTRICT, related_name="companies")
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name="companies")
+    logo = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=False)
 
-    #def __str___(self):
-    #    return f"{self.id} - {self.name} ({self.ticker})"
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Companies"
 
     def __str__(self):
         return f"{self.name}, {self.ticker}"
-
-    class Meta:
-        ordering = ['name']
-        verbose_name_plural = "Companies"
