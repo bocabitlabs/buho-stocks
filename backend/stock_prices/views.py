a2794e72a17565d53b5ee037024b202d1e5abc38
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-03-22 22:23:59 +0100

    Update stock prices urls and views

--- backend/stock_prices/views.py
+++ backend/stock_prices/views.py
@@ -2,15 +2,41 @@ import logging
 
 from companies.models import Company
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from settings.models import UserSettings
 from stock_prices.api import StockPricesApi
+from stock_prices.models import StockPrice
 from stock_prices.serializers import StockPriceSerializer
 from stock_prices.services.yfinance_api_client import YFinanceApiClient
 
 logger = logging.getLogger("buho_backend")
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+class ExchangeRateViewSet(viewsets.ModelViewSet):
+    """Get all the exchange rates from a user"""
+
+    pagination_class = StandardResultsSetPagination
+    serializer_class = StockPriceSerializer
+
+    def get_queryset(self):
+        sort_by = self.request.query_params.get("sort_by", "transaction_date")
+        order_by = self.request.query_params.get("order_by", "desc")
+        # Sort and order the queryset
+        if order_by == "desc":
+            queryset = StockPrice.objects.order_by(f"-{sort_by}")
+        else:
+            queryset = StockPrice.objects.order_by(f"{sort_by}")
+
+        return queryset
 
 
 class StockPricesYearAPIView(APIView):
