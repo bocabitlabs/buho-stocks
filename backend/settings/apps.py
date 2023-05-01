8f9d784eb511cdc69b809f741006c97af51ed6ec
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-05-01 10:41:50 +0200

    Remove old settings.signals

--- backend/settings/apps.py
+++ backend/settings/apps.py
@@ -2,12 +2,5 @@ from django.apps import AppConfig
 
 
 class SettingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'settings'
-
-    # add this
-    def ready(self):
-        import settings.signals  # noqa
-
-
-
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "settings"
