d184614af4e966e11f2e3d158f2b23824ed9448e
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-05-01 15:45:22 +0200

    Update the company stats after a transaction

--- backend/rights_transactions/views.py
+++ backend/rights_transactions/views.py
@@ -1,9 +1,15 @@
+import logging
+from datetime import datetime
+
 from companies.models import Company
 from django_filters.rest_framework import DjangoFilterBackend
 from log_messages.models import LogMessage
 from rest_framework import viewsets
 from rights_transactions.models import RightsTransaction
 from rights_transactions.serializers import RightsTransactionSerializer
+from stats.utils.company_stats_utils import CompanyStatsUtils
+
+logger = logging.getLogger("buho_backend")
 
 
 class RightsViewSet(viewsets.ModelViewSet):
@@ -18,6 +24,16 @@ class RightsViewSet(viewsets.ModelViewSet):
         serializer.save()
         # Log the operation
         company = Company.objects.get(id=serializer.data["company"])
+        self.create_add_rights_log_message(serializer, company)
+        self.add_rights_update_company_stats(serializer, company)
+
+    def add_rights_update_company_stats(self, serializer, company):
+        logger.debug(f"Updating company stats for {company.name} after adding rights")
+        transaction_date = datetime.strptime(serializer.data.get("transaction_date"), "%Y-%m-%d")
+        company_stats = CompanyStatsUtils(company.id, year=transaction_date.year, update_api_price=True)
+        company_stats.get_stats_for_year()
+
+    def create_add_rights_log_message(self, serializer, company):
         LogMessage.objects.create(
             message_type=LogMessage.MESSAGE_TYPE_ADD_RIGHTS,
             message_text=(
--- backend/shares_transactions/views.py
+++ backend/shares_transactions/views.py
@@ -1,9 +1,15 @@
+import logging
+from datetime import datetime
+
 from companies.models import Company
 from django_filters.rest_framework import DjangoFilterBackend
 from log_messages.models import LogMessage
 from rest_framework import viewsets
 from shares_transactions.models import SharesTransaction
 from shares_transactions.serializers import SharesTransactionSerializer
+from stats.utils.company_stats_utils import CompanyStatsUtils
+
+logger = logging.getLogger("buho_backend")
 
 
 class SharesViewSet(viewsets.ModelViewSet):
@@ -18,6 +24,16 @@ class SharesViewSet(viewsets.ModelViewSet):
         serializer.save()
         # Log the operation
         company = Company.objects.get(id=serializer.data["company"])
+        self.add_shares_update_company_stats(serializer, company)
+        self.create_add_shares_log_message(serializer, company)
+
+    def add_shares_update_company_stats(self, serializer, company):
+        logger.debug(f"Updating company stats for {company.name} after adding shares")
+        transaction_date = datetime.strptime(serializer.data.get("transaction_date"), "%Y-%m-%d")
+        company_stats = CompanyStatsUtils(company.id, year=transaction_date.year, update_api_price=True)
+        company_stats.get_stats_for_year()
+
+    def create_add_shares_log_message(self, serializer, company):
         LogMessage.objects.create(
             message_type=LogMessage.MESSAGE_TYPE_ADD_SHARES,
             message_text=(
