8a0e570951a9b50c00b1d62ad1f0f4b40ea000da
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-08-04 18:06:48 +0200

    Improve company details responsiveness

--- client/src/pages/companies/CompanyDetailsPage/components/CompanyDetailsPageHeader/CompanyDetailsPageHeader.tsx
+++ client/src/pages/companies/CompanyDetailsPage/components/CompanyDetailsPageHeader/CompanyDetailsPageHeader.tsx
@@ -1,9 +1,9 @@
 import React, { ReactNode, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
-import { DeleteOutlined, EditOutlined, LinkOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { PageHeader } from "@ant-design/pro-layout";
-import { Button, Popconfirm, Tag, Typography, theme } from "antd";
+import { Button, Popconfirm, Typography, theme } from "antd";
 import breadCrumbRender from "breadcrumbs";
 import CountryFlag from "components/CountryFlag/CountryFlag";
 import { useDeleteCompany } from "hooks/use-companies/use-companies";
@@ -11,22 +11,16 @@ import CompanyAddEditForm from "pages/companies/CompanyDetailsPage/components/Co
 
 interface Props {
   companyName: string;
-  companyTicker: string;
-  companyLogo: string | undefined;
   companyCountryCode: string;
   portfolioName: string;
-  companyUrl: string;
   children: ReactNode;
 }
 const { useToken } = theme;
 
 function CompanyDetailsPageHeader({
   companyName,
-  companyTicker,
-  companyLogo,
-  companyUrl,
-  companyCountryCode,
   portfolioName,
+  companyCountryCode,
   children,
 }: Props) {
   const { t } = useTranslation();
@@ -66,23 +60,19 @@ function CompanyDetailsPageHeader({
     <PageHeader
       className="site-page-header"
       style={{ background: token.colorBgContainer }}
+      tags={[
+        <CountryFlag
+          code={companyCountryCode}
+          key={companyCountryCode}
+          width={20}
+        />,
+      ]}
       title={<Typography.Title level={2}>{companyName}</Typography.Title>}
-      subTitle={companyTicker}
-      avatar={{ src: companyLogo }}
       breadcrumb={{ items: routes }}
       breadcrumbRender={breadCrumbRender}
-      tags={[
-        <CountryFlag code={companyCountryCode} key={companyCountryCode} />,
-        <Tag color="blue" key="url" style={{ marginLeft: 16 }}>
-          <a href={companyUrl} target="_blank" rel="noopener noreferrer">
-            <LinkOutlined />
-          </a>
-        </Tag>,
-      ]}
       extra={[
         <Button
           key="company-edit-header"
-          type="text"
           icon={<EditOutlined />}
           onClick={() => showModal()}
         />,
@@ -93,7 +83,7 @@ function CompanyDetailsPageHeader({
           okText={t("Yes")}
           cancelText={t("No")}
         >
-          <Button icon={<DeleteOutlined />} type="text" danger />
+          <Button icon={<DeleteOutlined />} danger />
         </Popconfirm>,
       ]}
     >
