bbbd6c4ba33276dc7480dcb71d212ce3777f8ba7
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-08-04 18:08:17 +0200

    Add width parameter to country flag

--- client/src/components/CountryFlag/CountryFlag.tsx
+++ client/src/components/CountryFlag/CountryFlag.tsx
@@ -3,14 +3,24 @@ import Flags from "country-flag-icons/react/3x2";
 
 interface Props {
   code: string;
+  width?: number;
 }
 
-export default function CountryFlag({ code }: Props) {
+export default function CountryFlag({ code, width }: Props) {
   let countryCode = code;
   if (code === "EUR") {
     countryCode = "EU";
   }
   if (!countryCode || !hasFlag(countryCode.toUpperCase())) return null;
   const FlagComponent = Flags[countryCode.toUpperCase() as keyof typeof Flags];
-  return <FlagComponent data-testid="country-flag" style={{ maxHeight: 20 }} />;
+  return (
+    <FlagComponent
+      data-testid="country-flag"
+      style={{ maxHeight: 20, width }}
+    />
+  );
 }
+
+CountryFlag.defaultProps = {
+  width: undefined,
+};
