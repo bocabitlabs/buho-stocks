71055580c02bb560b10d9e77bf4931fc3790968a
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-10-26 18:01:29 +0200

    Add reconnect button for socket

--- client/src/components/TopNavBar/components/TasksModal/TasksModal.tsx
+++ client/src/components/TopNavBar/components/TasksModal/TasksModal.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { LoadingOutlined, RightSquareOutlined } from "@ant-design/icons";
+import {
+  ApiOutlined,
+  LoadingOutlined,
+  RightSquareOutlined,
+} from "@ant-design/icons";
 import { Button, Modal, Progress, Space, Typography } from "antd";
 import { useSettings } from "hooks/use-settings/use-settings";
 import { ITaskResult } from "types/task-result";
@@ -8,6 +12,7 @@ import { ITaskResult } from "types/task-result";
 function TasksModal() {
   // List of tasks and their statuses
   const [tasks, setTasks] = useState<ITaskResult[]>([]);
+  const [connected, setConnected] = useState(false);
   const { t } = useTranslation();
   const ws = useRef<any>(null);
 
@@ -67,6 +72,24 @@ function TasksModal() {
     return "active";
   };
 
+  const reconnectSocket = () => {
+    if (!settings) return;
+    // If using https, replace ws with wss
+    const wsUrl = `ws://${settings.backendHostname}/ws/tasks/`;
+    // Replace ws with wss for https
+    if (document.location.protocol === "https:") {
+      wsUrl.replace("ws://", "wss://");
+    }
+    console.log(`Connecting to websocket`);
+    console.log(wsUrl);
+    try {
+      ws.current = new WebSocket(wsUrl);
+    } catch (e) {
+      console.log(e);
+      setConnected(false);
+    }
+  };
+
   useEffect(() => {
     if (settings) {
       // If using https, replace ws with wss
@@ -77,7 +100,12 @@ function TasksModal() {
       }
       console.log(`Connecting to websocket`);
       console.log(wsUrl);
-      ws.current = new WebSocket(wsUrl);
+      try {
+        ws.current = new WebSocket(wsUrl);
+      } catch (e) {
+        console.log(e);
+        setConnected(false);
+      }
     }
 
     const wsCurrent = ws.current;
@@ -94,10 +122,12 @@ function TasksModal() {
 
     ws.current.onopen = (event: any) => {
       console.log("Connected", event);
+      setConnected(true);
     };
 
     ws.current.onclose = (event: any) => {
       console.log("Closed", event);
+      setConnected(false);
     };
 
     ws.current.onmessage = (event: any) => {
@@ -113,54 +143,65 @@ function TasksModal() {
     };
   }, []);
 
+  if (connected) {
+    return (
+      <>
+        <Button
+          style={{ marginLeft: 10 }}
+          type="default"
+          icon={
+            tasks.filter(
+              (task: ITaskResult) =>
+                task.status !== "COMPLETED" && task.status !== "FAILED",
+            ).length > 0 ? (
+              <LoadingOutlined />
+            ) : (
+              <RightSquareOutlined />
+            )
+          }
+          onClick={showTasksModal}
+        />
+        <Modal
+          title={t("Tasks")}
+          open={isTasksModalOpen}
+          onCancel={handleTasksOk}
+          footer={[
+            <Button key="clear" onClick={clearCompletedTasksList}>
+              {t("Clear completed tasks")}
+            </Button>,
+            <Button key="submit" type="primary" onClick={handleTasksOk}>
+              {t("Close")}
+            </Button>,
+          ]}
+        >
+          <Space direction="vertical" style={{ display: "flex" }}>
+            {tasks.length === 0 && <Typography.Text>No tasks</Typography.Text>}
+            {tasks.length > 0 &&
+              tasks.map((task: ITaskResult) => (
+                <div key={task.task_id}>
+                  <Typography.Text>{task.task_name}:</Typography.Text>
+                  <Progress
+                    percent={task.progress}
+                    status={getProgressStatus(task.status)}
+                  />
+                  <Typography.Text type="secondary">
+                    {task.details}
+                  </Typography.Text>
+                </div>
+              ))}
+          </Space>
+        </Modal>
+      </>
+    );
+  }
+
   return (
-    <>
-      <Button
-        style={{ marginLeft: 10 }}
-        type="default"
-        icon={
-          tasks.filter(
-            (task: ITaskResult) =>
-              task.status !== "COMPLETED" && task.status !== "FAILED",
-          ).length > 0 ? (
-            <LoadingOutlined />
-          ) : (
-            <RightSquareOutlined />
-          )
-        }
-        onClick={showTasksModal}
-      />
-      <Modal
-        title={t("Tasks")}
-        open={isTasksModalOpen}
-        onCancel={handleTasksOk}
-        footer={[
-          <Button key="clear" onClick={clearCompletedTasksList}>
-            {t("Clear completed tasks")}
-          </Button>,
-          <Button key="submit" type="primary" onClick={handleTasksOk}>
-            {t("Close")}
-          </Button>,
-        ]}
-      >
-        <Space direction="vertical" style={{ display: "flex" }}>
-          {tasks.length === 0 && <Typography.Text>No tasks</Typography.Text>}
-          {tasks.length > 0 &&
-            tasks.map((task: ITaskResult) => (
-              <div key={task.task_id}>
-                <Typography.Text>{task.task_name}:</Typography.Text>
-                <Progress
-                  percent={task.progress}
-                  status={getProgressStatus(task.status)}
-                />
-                <Typography.Text type="secondary">
-                  {task.details}
-                </Typography.Text>
-              </div>
-            ))}
-        </Space>
-      </Modal>
-    </>
+    <Button
+      onClick={reconnectSocket}
+      title="Reconnect to get the tasks updates"
+    >
+      <ApiOutlined />
+    </Button>
   );
 }
 
