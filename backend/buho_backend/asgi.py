5fc2902fd4dca354ffd5341dd3f15abdf39fb335
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-10-26 18:00:33 +0200

    Run the application in production using daphne as server

--- backend/buho_backend/asgi.py
+++ backend/buho_backend/asgi.py
@@ -9,15 +9,24 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from buho_backend.urls import websocket_urlpatterns
+from buho_backend import consumers
+from buho_backend.settings_loader import get_settings_module
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buho_backend.settings")
+settings_module = get_settings_module()
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+# print(settings_module)
 django_asgi_app = get_asgi_application()
+
+websocket_urlpatterns = [
+    re_path(r"ws/tasks/$", consumers.TaskConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter(
     {
