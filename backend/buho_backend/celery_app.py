9f4712c6af2be70ba04f932936368c658557e3d5
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2023-11-02 18:15:53 +0100

    Rewrite the revoke task code

--- backend/buho_backend/celery_app.py
+++ backend/buho_backend/celery_app.py
@@ -3,6 +3,7 @@ import os
 
 from buho_backend.settings_loader import get_settings_module
 from celery import Celery
+from celery.app.control import Control
 
 settings_module = get_settings_module()
 
@@ -10,7 +11,7 @@ settings_module = get_settings_module()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 app = Celery("buho_celery")
-
+control = Control(app=app)
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
@@ -24,13 +25,11 @@ logger = logging.getLogger("buho_backend")
 
 
 def revoke_scheduled_tasks_by_name(task_name):
-    i = app.control.inspect()
-
     # Get all active tasks
-    scheduled_tasks = i.scheduled()
+    scheduled_tasks = control.inspect().scheduled()
 
-    for worker, tasks in scheduled_tasks.items():
+    for queue, tasks in scheduled_tasks.items():
         for task in tasks:
             # If the task name matches, revoke the task
             if task["name"] == task_name:
-                i.revoke(task["id"], terminate=True)
+                control.revoke(task["id"], terminate=True)
