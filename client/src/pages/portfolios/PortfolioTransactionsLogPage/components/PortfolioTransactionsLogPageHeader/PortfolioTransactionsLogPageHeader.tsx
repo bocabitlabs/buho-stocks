8a42f1cb055027afa922d7d27e1f5ce7f7d8eea0
Rene Fernandez <rene.fernandez@cern.ch>
2024-07-31 07:34:45 +0200

    Use mantine in portfolio logs page

--- client/src/pages/portfolios/PortfolioTransactionsLogPage/components/PortfolioTransactionsLogPageHeader/PortfolioTransactionsLogPageHeader.tsx
+++ client/src/pages/portfolios/PortfolioTransactionsLogPage/components/PortfolioTransactionsLogPageHeader/PortfolioTransactionsLogPageHeader.tsx
@@ -1,59 +1,75 @@
-import React, { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
-import { PageHeader } from "@ant-design/pro-layout";
-import { Spin, Typography, theme } from "antd";
-import breadCrumbRender from "breadcrumbs";
-import CountryFlag from "components/CountryFlag/CountryFlag";
+import {
+  Anchor,
+  Breadcrumbs,
+  Group,
+  Loader,
+  Stack,
+  Title,
+} from "@mantine/core";
 
 interface Props {
   portfolioName: string;
-  portfolioCountryCode: string;
-  children: ReactNode;
 }
-const { useToken } = theme;
 
-function PortfolioDetailsPageHeader({
-  portfolioName,
-  portfolioCountryCode,
-  children,
-}: Props) {
+function PortfolioDetailsPageHeader({ portfolioName }: Props) {
   const { id } = useParams();
   const { t } = useTranslation();
-  const { token } = useToken();
   const routes = [
     {
       href: "/home",
       title: t("Home"),
+      id: "home",
     },
     {
       href: `/portfolios/${id}`,
       title: portfolioName,
+      id: "portfolio",
     },
     {
       href: `/portfolios/${id}/log`,
       title: t("Log"),
+      id: "log",
     },
-  ];
+  ].map((item) => (
+    <Anchor href={item.href} key={item.id}>
+      {item.title}
+    </Anchor>
+  ));
   if (!portfolioName) {
-    return <Spin />;
+    return <Loader />;
   }
   return (
-    <PageHeader
-      className="site-page-header"
-      style={{
-        background: token.colorBgContainer,
-      }}
-      title={<Typography.Title level={2}>{portfolioName}</Typography.Title>}
-      subTitle={t("Portfolio transactions log")}
-      breadcrumb={{ items: routes }}
-      breadcrumbRender={breadCrumbRender}
-      tags={[
-        <CountryFlag code={portfolioCountryCode} key={portfolioCountryCode} />,
-      ]}
-    >
-      {children}
-    </PageHeader>
+    <Stack>
+      <Breadcrumbs>{routes}</Breadcrumbs>
+
+      <Group justify="space-between">
+        <Stack>
+          <Title order={1} textWrap="pretty">
+            {t("Portfolio transactions log")}
+          </Title>
+          <Title order={2} textWrap="pretty">
+            {portfolioName}
+          </Title>
+        </Stack>
+      </Group>
+    </Stack>
+    // <PageHeader
+    //   className="site-page-header"
+    //   style={{
+    //     background: token.colorBgContainer,
+    //   }}
+    //   title={<Typography.Title level={2}>{portfolioName}</Typography.Title>}
+    //   subTitle={t("Portfolio transactions log")}
+    //   breadcrumb={{ items: routes }}
+    //   breadcrumbRender={breadCrumbRender}
+    //   tags={[
+    //     <CountryFlag code={portfolioCountryCode} key={portfolioCountryCode} />,
+    //   ]}
+    // >
+    //   {children}
+    // </PageHeader>
   );
 }
 
