b175845e186ed7fef71277c522b14c5268a4535e
Rene Fernandez <rene.fernandez@cern.ch>
2024-07-31 07:29:59 +0200

    Remove antd from Benchmarks and use mantine

--- client/src/pages/benchmarks/BenchmarksListPage/BenchmarksListPage.tsx
+++ client/src/pages/benchmarks/BenchmarksListPage/BenchmarksListPage.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import { Grid } from "@mantine/core";
 import BenchmarksListPageHeader from "./components/BenchmarksListPageHeader/BenchmarksListPageHeader";
 import BenchmarksListTable from "./components/BenchmarksListTable/BenchmarksListTable";
 
 export default function BenchmarksListPage() {
   return (
-    <BenchmarksListPageHeader>
-      <BenchmarksListTable />
-    </BenchmarksListPageHeader>
+    <Grid p={20}>
+      <Grid.Col span={12}>
+        <BenchmarksListPageHeader />
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <BenchmarksListTable />
+      </Grid.Col>
+    </Grid>
   );
 }
---
+++ client/src/pages/benchmarks/BenchmarksListPage/components/BenchmarkYearForm/BenchmarkYearForm.tsx
@@ -0,0 +1,68 @@
+import { useTranslation } from "react-i18next";
+import { Button, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IBenchmarkYearFormFields } from "types/benchmark";
+
+interface AddFormProps {
+  benchmarkId: number;
+  onSubmitCallback: (values: IBenchmarkYearFormFields) => void;
+}
+
+function BenchmarkYearForm({ benchmarkId, onSubmitCallback }: AddFormProps) {
+  const { t } = useTranslation();
+
+  const form = useForm<IBenchmarkYearFormFields>({
+    mode: "uncontrolled",
+    initialValues: {
+      year: new Date().getFullYear(),
+      value: 0,
+      valueCurrency: "",
+      returnPercentage: 0,
+      benchmark: benchmarkId,
+    },
+  });
+
+  const onSubmit = (values: any) => {
+    onSubmitCallback(values);
+  };
+
+  return (
+    <div>
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          withAsterisk
+          label={t("Year")}
+          key={form.key("year")}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...form.getInputProps("year")}
+        />
+        <TextInput
+          withAsterisk
+          label={t("Value")}
+          key={form.key("value")}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...form.getInputProps("value")}
+        />
+        <TextInput
+          withAsterisk
+          label={t("Value currency")}
+          key={form.key("valueCurrency")}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...form.getInputProps("valueCurrency")}
+        />
+        <TextInput
+          withAsterisk
+          label={t("Return Percentage")}
+          key={form.key("returnPercentage")}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...form.getInputProps("returnPercentage")}
+        />
+        <Button type="submit" color="blue" mt="md">
+          {t("Add benchmark year")}
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default BenchmarkYearForm;
