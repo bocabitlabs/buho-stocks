11f633db6c0884446f7b5bfd63090321005a54ff
Rene Fernandez <rene.fernandez@cern.ch>
2024-07-31 07:30:39 +0200

    Use mantine on currencies pages

---
+++ client/src/pages/currencies/CurrenciesListPage/components/CurrencyForm/CurrencyFormProvider.tsx
@@ -0,0 +1,62 @@
+import { useTranslation } from "react-i18next";
+import { Alert, Loader } from "@mantine/core";
+import CurrencyForm from "./CurrencyForm";
+import {
+  useAddCurrency,
+  useCurrency,
+  useUpdateCurrency,
+} from "hooks/use-currencies/use-currencies";
+
+type Props = {
+  id?: number;
+  isUpdate?: boolean;
+  isVisible: boolean;
+  onCloseCallback: () => void;
+};
+
+export default function CurrencyFormProvider({
+  id = undefined,
+  isUpdate = false,
+  isVisible,
+  onCloseCallback,
+}: Props) {
+  const { t } = useTranslation();
+  const { data, error, isLoading, isError } = useCurrency(id);
+
+  const { mutate: createCurrency } = useAddCurrency({
+    onSuccess: onCloseCallback,
+  });
+  const { mutate: updateCurrency } = useUpdateCurrency({
+    onSuccess: onCloseCallback,
+  });
+
+  const onSubmitCallback = (values: any) => {
+    if (isUpdate) {
+      updateCurrency({ id, newCurrency: values });
+    } else {
+      createCurrency(values);
+    }
+  };
+
+  if (isLoading) {
+    return <Loader color="blue" type="dots" />;
+  }
+
+  if (isError) {
+    return (
+      <Alert title={t("Unable to load currency")} color="red">
+        {error?.message}
+      </Alert>
+    );
+  }
+
+  return (
+    <CurrencyForm
+      currency={data}
+      isVisible={isVisible}
+      onCloseCallback={onCloseCallback}
+      onSubmitCallback={onSubmitCallback}
+      isUpdate={isUpdate}
+    />
+  );
+}
