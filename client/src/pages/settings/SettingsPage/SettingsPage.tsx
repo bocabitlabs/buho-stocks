7c5e867626e959d711563bb30531e104544216de
Rene Fernandez <rene.fernandez@cern.ch>
2024-07-31 07:35:39 +0200

    Use mantine in settings

--- client/src/pages/settings/SettingsPage/SettingsPage.tsx
+++ client/src/pages/settings/SettingsPage/SettingsPage.tsx
@@ -1,11 +1,17 @@
 import React, { ReactElement } from "react";
+import { Grid } from "@mantine/core";
 import SettingsForm from "./components/SettingsForm/SettingsForm";
 import SettingsPageHeader from "./components/SettingsHeader/SettingsHeader";
 
 export default function SettingsPage(): ReactElement {
   return (
-    <SettingsPageHeader>
-      <SettingsForm />
-    </SettingsPageHeader>
+    <Grid p={20}>
+      <Grid.Col span={12}>
+        <SettingsPageHeader />
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <SettingsForm />
+      </Grid.Col>
+    </Grid>
   );
 }
