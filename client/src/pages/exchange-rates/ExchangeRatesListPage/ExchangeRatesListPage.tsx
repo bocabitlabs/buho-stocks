4b55efb1338368771150953e809f910e7d922cb9
Rene Fernandez <rene.fernandez@cern.ch>
2024-07-31 07:31:34 +0200

    Use mantine in exchange rates pages

--- client/src/pages/exchange-rates/ExchangeRatesListPage/ExchangeRatesListPage.tsx
+++ client/src/pages/exchange-rates/ExchangeRatesListPage/ExchangeRatesListPage.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import { Grid } from "@mantine/core";
 import ExchangeRatesListTable from "./components/ExchangeRatesListTable/ExchangeRatesListTable";
 import ExchangeRatesPageHeader from "./components/ExchangeRatesPageHeader/ExchangeRatesPageHeader";
 
 export default function ExchangeRatesListPage() {
   return (
-    <ExchangeRatesPageHeader>
-      <ExchangeRatesListTable />
-    </ExchangeRatesPageHeader>
+    <Grid p={20}>
+      <Grid.Col span={12}>
+        <ExchangeRatesPageHeader />
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <ExchangeRatesListTable />
+      </Grid.Col>
+    </Grid>
   );
 }
