9dcad003b3203687bceb825d35c79894ce152d82
Rene Fernandez <rene.fernandez@cern.ch>
2024-08-16 22:35:38 +0200

    chore: Update logging configuration to include file handlers

--- backend/buho_backend/settings.py
+++ backend/buho_backend/settings.py
@@ -254,56 +254,87 @@ LOGGER_HANDLERS = config(
     "LOGGER_HANDLERS", cast=lambda v: [s.strip() for s in v.split(",")]
 )
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "file": {
-            "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
-            "style": "{",
+if config("LOG_TO_FILE", default=False, cast=bool):
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "file": {
+                "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
+                "style": "{",
+            },
+            "console": {
+                "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
+                "style": "{",
+            },
         },
-        "console": {
-            "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
-            "style": "{",
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "console"},
+            "debug_file": {
+                "level": "DEBUG",
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "file",
+                "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
+                "backupCount": 10,
+                "filename": os.path.join(LOGS_ROOT, "debug.log"),
+            },
+            "info_file": {
+                "level": "INFO",
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "file",
+                "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
+                "backupCount": 10,
+                "filename": os.path.join(LOGS_ROOT, "info.log"),
+            },
+            "error_file": {
+                "level": "ERROR",
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "file",
+                "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
+                "backupCount": 10,
+                "filename": os.path.join(LOGS_ROOT, "error.log"),
+            },
         },
-    },
-    "handlers": {
-        "console": {"class": "logging.StreamHandler", "formatter": "console"},
-        "debug_file": {
-            "level": "DEBUG",
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "file",
-            "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
-            "backupCount": 10,
-            "filename": os.path.join(LOGS_ROOT, "debug.log"),
+        "loggers": {
+            "*": {
+                "handlers": LOGGER_HANDLERS,
+                "level": "ERROR",
+                "propagate": True,
+            },
+            "buho_backend": {
+                "handlers": LOGGER_HANDLERS,
+                "level": LOG_LEVEL,
+                "propagate": True,
+            },
         },
-        "info_file": {
-            "level": "INFO",
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "file",
-            "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
-            "backupCount": 10,
-            "filename": os.path.join(LOGS_ROOT, "info.log"),
+    }
+else:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "file": {
+                "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
+                "style": "{",
+            },
+            "console": {
+                "format": "{levelname} | {asctime} | {message} | {pathname}:{lineno} | {module} | {funcName}",  # noqa
+                "style": "{",
+            },
         },
-        "error_file": {
-            "level": "ERROR",
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "file",
-            "maxBytes": 15728640,  # 1024 * 1024 * 15B = 15MB
-            "backupCount": 10,
-            "filename": os.path.join(LOGS_ROOT, "error.log"),
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "console"},
         },
-    },
-    "loggers": {
-        "*": {
-            "handlers": LOGGER_HANDLERS,
-            "level": "ERROR",
-            "propagate": True,
+        "loggers": {
+            "*": {
+                "handlers": LOGGER_HANDLERS,
+                "level": "ERROR",
+                "propagate": True,
+            },
+            "buho_backend": {
+                "handlers": LOGGER_HANDLERS,
+                "level": LOG_LEVEL,
+                "propagate": True,
+            },
         },
-        "buho_backend": {
-            "handlers": LOGGER_HANDLERS,
-            "level": LOG_LEVEL,
-            "propagate": True,
-        },
-    },
-}
+    }
