f6fb05465358699bbac2be1c5779b4ff71dff292
Rene Fernandez <rene.fernandez@cern.ch>
2024-08-24 09:28:41 +0200

    Refactor PortfolioCard and add tests

--- client/src/types/market.ts
+++ client/src/types/market.ts
@@ -5,7 +5,6 @@ export interface ITimezone {
 export interface IMarketFormFields {
   name: string;
   description: string;
-  color: string;
   region: string;
   openTime: Date | string;
   closeTime: Date | string;
