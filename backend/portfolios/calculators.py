e27a49a40cba185300a88abfb75407cd7ac6d053
Rene Fernandez <rene.fernandez@cern.ch>
2024-09-02 20:50:54 +0200

    Get portfolio first year for open and closed companies

--- backend/portfolios/calculators.py
+++ backend/portfolios/calculators.py
@@ -6,9 +6,9 @@ logger = logging.getLogger("buho_backend")
 
 
 def get_portfolio_first_year(portfolio_id):
-    query = SharesTransaction.objects.filter(
-        company__portfolio=portfolio_id, company__is_closed=False
-    ).order_by("transaction_date")
+    query = SharesTransaction.objects.filter(company__portfolio=portfolio_id).order_by(
+        "transaction_date"
+    )
     if query.exists():
         return query[0].transaction_date.year
     return None
